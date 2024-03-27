79020299b6b347d748b9f57cd468c63a14c2c7ec
Thamer <tamersakly@gmail.com>
2024-03-27 16:51:35 +0100

    first commit

---
+++ src/WindyMap.js
@@ -0,0 +1,13 @@
+function WindyMap() {
+    return (
+      <div >
+        <header >
+          <h1>Map</h1>
+        </header>
+         {/* Utilisez le composant WindyMap ici */}
+      </div>
+    );
+  }
+  
+  export default WindyMap;
+  

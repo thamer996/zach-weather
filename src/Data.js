4dd117a5ecd11af890cadc22f2472d6ef3f090dd
salah sammari <salahsammari6@gmail.com>
2024-04-03 15:53:59 +0100

    affichage data

--- src/Data.js
+++ src/Data.js
@@ -8,6 +8,9 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import axios from "axios";
+
 import {
   faCloudShowersHeavy,
   faSnowflake,
@@ -26,6 +29,47 @@ function Data() {
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
+  const [selectedOption, setSelectedOption] = useState(""); // Maintient l'option sélectionnée
+
+  const [weatherData, setWeatherData] = useState([]);
+  const options = [
+    "C Coast North",
+    "C Coast South",
+    "C Coast Tip",
+    "C Corpus Christi",
+    "E East Ex",
+    "Houston Central",
+    "Houston North",
+    "Houston South",
+    "N Far East",
+    "N Seymour",
+    "N Sherman",
+    "N Wichita Falls",
+    "NC Dallas Central",
+    "NC Dallas East",
+    "NC Dallas South",
+    "NC Fort Worth",
+    "NC West",
+    "PH Amarillo NE",
+    "PH Amarillo NW",
+    "PH Amarillo S",
+    "PH Plainview",
+    "S NELOB",
+    "S NELOB 2",
+    "S San Antonio",
+    "SC Austin Central",
+    "SC Austin South",
+    "SC Killeen",
+    "W Abilene",
+    "W Bigspring",
+    "W Brady",
+    "W Del Rio",
+    "W Far Fort Stockton",
+    "W Lubbock",
+    "W McCamey",
+    "W Odessa",
+    "W San Angelo",
+  ];
 
   const currentDate = new Date();
 
@@ -48,6 +92,38 @@ function Data() {
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
+  };
+
+  const handleChange = async (event) => {
+    const selectedPointName = event.target.value;
+    setSelectedOption(selectedPointName);
+    if (selectedPointName) {
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/download-weather",
+          {
+            point_name: selectedPointName,
+          }
+        );
+        if (
+          response.data &&
+          response.data.days &&
+          Array.isArray(response.data.days)
+        ) {
+          // Extraction des données météorologiques à partir du tableau 'days'
+          const weatherDataFromDays = response.data.days
+            .map((day) => day.hours)
+            .flat();
+          setWeatherData(weatherDataFromDays);
+        } else {
+          console.error("Invalid weather data format:", response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    } else {
+      setWeatherData([]);
+    }
   };
 
   const columns = [
@@ -393,6 +469,364 @@ function Data() {
       align: "right",
       format: (value) => value.toLocaleString("en-US"),
     },
+    {
+      id: "zero",
+      label: "0",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "next day",
+      label: "|",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+
+    {
+      id: "one",
+      label: "1",
+      minWidth: 15, // Adjusted minWidth for Population
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "two",
+      label: "2",
+      minWidth: 15, // Adjusted minWidth for Population
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "three",
+      label: "3",
+      minWidth: 15, // Adjusted minWidth for Size
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "four",
+      label: "4	",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "five",
+      label: "5",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "six",
+      label: "6",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "seven",
+      label: "7",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "height",
+      label: "8",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "nine",
+      label: "9",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "ten",
+      label: "10",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "eleven",
+      label: "11",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "twelve",
+      label: "12",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "therteen",
+      label: "13",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "fourteen",
+      label: "14",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "fifteen",
+      label: "15",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "sixteen",
+      label: "16",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "seventeen",
+      label: "17",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "heighteen",
+      label: "18",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "nineteen",
+      label: "19",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "twenty",
+      label: "20",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "twentyone",
+      label: "21",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+
+    {
+      id: "twentytwo",
+      label: "22",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+
+    {
+      id: "twentythree",
+      label: "23",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+
+    {
+      id: "zero",
+      label: "0",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "next day",
+      label: "|",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+
+    {
+      id: "one",
+      label: "1",
+      minWidth: 15, // Adjusted minWidth for Population
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "two",
+      label: "2",
+      minWidth: 15, // Adjusted minWidth for Population
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "three",
+      label: "3",
+      minWidth: 15, // Adjusted minWidth for Size
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "four",
+      label: "4	",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "five",
+      label: "5",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "six",
+      label: "6",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "seven",
+      label: "7",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "height",
+      label: "8",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "nine",
+      label: "9",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "ten",
+      label: "10",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "eleven",
+      label: "11",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "twelve",
+      label: "12",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "therteen",
+      label: "13",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "fourteen",
+      label: "14",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "fifteen",
+      label: "15",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "sixteen",
+      label: "16",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "seventeen",
+      label: "17",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "heighteen",
+      label: "18",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "nineteen",
+      label: "19",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "twenty",
+      label: "20",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+    {
+      id: "twentyone",
+      label: "21",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+
+    {
+      id: "twentytwo",
+      label: "22",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+
+    {
+      id: "twentythree",
+      label: "23",
+      minWidth: 15, // Adjusted minWidth for Density
+      align: "right",
+      format: (value) => value.toLocaleString("en-US"),
+    },
+
     {
       id: "zero",
       label: "0",
@@ -1078,9 +1512,7 @@ function Data() {
       "13°"
     ),
     createData(
-      <span className="badge badge-success">
-        <FontAwesomeIcon icon={faTemperatureHigh} className="custom-icon" /> T°
-      </span>,
+      <span className="badge badge-success">cloudcover</span>,
       "13°",
       "13°",
       "13°",
@@ -1107,9 +1539,7 @@ function Data() {
       "13°"
     ),
     createData(
-      <span className="badge badge-success">
-        <FontAwesomeIcon icon={faTemperatureHigh} className="custom-icon" /> T°
-      </span>,
+      <span className="badge badge-success">visibility</span>,
       "13°",
       "13°",
       "13°",
@@ -1136,9 +1566,7 @@ function Data() {
       "13°"
     ),
     createData(
-      <span className="badge badge-success">
-        <FontAwesomeIcon icon={faTemperatureHigh} className="custom-icon" /> T°
-      </span>,
+      <span className="badge badge-success">solarradiation</span>,
       "13°",
       "13°",
       "13°",
@@ -1165,38 +1593,7 @@ function Data() {
       "13°"
     ),
     createData(
-      <span className="badge badge-success">
-        <FontAwesomeIcon icon={faTemperatureHigh} className="custom-icon" /> T°
-      </span>,
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°",
-      "13°"
-    ),
-    createData(
-      <span className="badge badge-success">
-        <FontAwesomeIcon icon={faTemperatureHigh} className="custom-icon" /> T°
-      </span>,
+      <span className="badge badge-success">solarenergy</span>,
       "13°",
       "13°",
       "13°",
@@ -1282,11 +1679,69 @@ function Data() {
 
   return (
     <Paper style={{ width: "100%", overflow: "auto" }}>
+      <FormControl fullWidth style={{ marginBottom: "20px" }}>
+        <InputLabel id="demo-simple-select-label">Select Country</InputLabel>
+        <div>
+          <select value={selectedOption} onChange={handleChange}>
+            <option value="">Select a location</option>
+            {options.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <div>
+            {weatherData.map((data, index) => (
+              <div key={index}>
+                <p>Date Time: {data.datetime}</p>
+                <p>windgust: {data.windgust}</p>
+                <p>winddir: {data.winddir}</p>
+                <p>winddir50: {data.winddir50}</p>
+                <p>winddir80: {data.winddir80}</p>
+                <p>winddir100: {data.winddir100}</p>
+                <p>windspeed: {data.windspeed}</p>
+                <p>windspeed50: {data.windspeed}</p>
+                <p>windspeed80: {data.windspeed80}</p>
+                <p>windspeed100: {data.windspeed100}</p>
+                <p>pressure: {data.pressure}</p>
+                <p>cloudcover: {data.cloudcover}</p>
+                <p>visibility: {data.visibility}</p>
+                <p>solarradiation: {data.solarradiation}</p>
+                <p>solarradiation: {data.solarradiation}</p>
+
+                {/* Add other data fields as needed */}
+              </div>
+            ))}
+          </div>
+        </div>
+      </FormControl>
       <TableContainer style={{ maxHeight: 800 }}>
         <Table stickyHeader aria-label="sticky table">
           <thead>
             <TableCell
               colSpan={columns.length / 2}
+              align="center"
+              style={{
+                fontWeight: "bold",
+                fontSize: "1.2em",
+                marginLeft: "200px",
+              }}
+            >
+              {`${getDayOfWeek(
+                currentDate
+              )}, ${currentDate.toLocaleDateString()}`}{" "}
+              {/* Display current day and date */}
+            </TableCell>
+            <TableCell
+              colSpan={columns.length}
+              align="center"
+              style={{ fontWeight: "bold", fontSize: "1.2em" }}
+            >
+              {`${getDayOfWeek(nextDay)}, ${nextDay.toLocaleDateString()}`}{" "}
+              {/* Display next day and date */}
+            </TableCell>
+            <TableCell
+              colSpan={columns.length / 4.5}
               align="center"
               style={{
                 fontWeight: "bold",
--- src/WindyMap.js
+++ src/WindyMap.js
@@ -1,19 +1,25 @@
 // WindyMap.js
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function WindyMap() {
-  const [mapUrl, setMapUrl] = useState('');
+  const [mapUrl, setMapUrl] = useState("");
 
   useEffect(() => {
     // Assuming Flask is running on localhost:5000
     // Adjust the URL as needed, especially in production
-    const url = 'http://localhost:5000/map';
+    const url = "http://localhost:5000/map";
     setMapUrl(url);
   }, []);
 
   return (
     <div>
-      {mapUrl && <iframe src={mapUrl} title="WindyMap" style={{width: '100%', height: '600px', border: 'none'}}></iframe>}
+      {mapUrl && (
+        <iframe
+          src={mapUrl}
+          title="WindyMap"
+          style={{ width: "100%", height: "600px", border: "none" }}
+        ></iframe>
+      )}
     </div>
   );
 }
